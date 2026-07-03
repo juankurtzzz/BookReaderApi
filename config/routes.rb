cd568d61f82fa701ebc65468d0e446a31322a0bf
juankurtzz <juanpietro1@outlook.com>
2026-07-03 13:04:24 -0300

    feat/add-new-routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,30 @@
 Rails.application.routes.draw do
-  resources :readings
-  resources :books
+  resources :readings do
+    collection do
+      get :reading_now
+      get :reading_finished
+    end
+
+    member do
+      get :current_reading
+      get :finished_reading
+    end
+  end
+
+  resources :books do 
+    collection do
+      get :search
+      get :categories
+      get :authors
+      get :long_books
+      get :short_books
+    end
+
+    member do 
+      get :category_by_id
+      get :author_by_id
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
